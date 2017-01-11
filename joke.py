66e02ca6e15eadf1bf3f443be259aa62e0ec4116
shriniv <voipsri@hotmail.com>
2017-01-10 17:01:45 -0800

    Add to the joke

--- joke.py
+++ joke.py
@@ -1,3 +1,11 @@
-'''This is a joke.'''
+#!/usr/bin/env python3
 
-print ('This is a joke, folks.')
+'''This is a joke.
+
+Hello this is a Joke description 
+
+from Shrini'''
+
+for x in range (0,10):
+    print ('This is a joke, folks.')
+
